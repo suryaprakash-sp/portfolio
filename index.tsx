7107bc197bda4f7ddcba2d26f61d74a5365fe9b4
Surya Prakash Manubolu <suryaprakash.sp987@gmail.com>
2025-12-07 11:30:43 +0530

    Fix: Add basename to BrowserRouter for GitHub Pages routing

--- index.tsx
+++ index.tsx
@@ -12,7 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/project/:projectId" element={<ProjectDetail />} />
